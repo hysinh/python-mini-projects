abbc2f5fbb0e4d5ad5cd48ddbddd989de5a8b3ad
NgocDzung Smith <hysinh1@gmail.com>
2024-05-04 13:12:00 +0000

    Converts letter in random word to a list. Prints random word with _ representing letters. Checks to see if the guessed letter is present in the word and prints result

--- hangman2.py
+++ hangman2.py
@@ -2,25 +2,39 @@ import random
 
 words = ['cat', 'pickle', 'sandwich', 'dolphin', 'popcorn', 'spaceship', 'grass']
 
-word_letters = [] #String of letters of the active_word
-used_letters = [] #string of used letters
+word_letters = [] #list of letters of the active_word
+blank_letters = []
+used_letters = [] #list of used letters
 correct_letters = []
 attempts = 8 
 
 def generate_random_word():
-    active_word = random.choice(words) #randomly selected word from words list
-    letters = [letter for letter in active_word]
-    blanks = ["_" for letter in letters]    
-    print(active_word)
-    return blanks
+    random_word = random.choice(words) #randomly selected word from words list
+    word_letters.append([letter for letter in random_word])
+    return random_word
+
+
+def makes_letter_list(word):
+    letters = [letter for letter in word]
+    return letters
     
-#def convert_to_dashes():
+    
+def makes_blanks(list):
+    blanks = ["_" for letter in list]    
+    return blanks
 
 
-#print each letter to "_" for each item in list word_letters
 
-#request guess in put from user
-#compare guess to each item in list of active_word - for loop
+def test_letter(letter, word):
+    print(word)
+    print(letter)
+    for i in word:
+        if letter == i:
+            print(f"this letter is in the word: {letter}")
+        else:
+            print(f"This letter is not in the word: {letter}")
+
+
 
 #if letter matches and letters in list, 
     # replace "_" with letter
@@ -44,9 +58,14 @@ def generate_random_word():
     #input(play again?)
 
 def run_game():
-    word_letters = generate_random_word()
-    print(*word_letters, sep = " ")
+    active_word = generate_random_word()
+    print(f"active_word: {active_word}")
+    print(word_letters)
+    letters = makes_letter_list(active_word)
+    blanks = makes_blanks(letters)
+    print(*blanks, sep = " ")
     guess = input("Guess a letter: \n").lower()
+    test_letter(guess, letters)
 
 
 print("Welcome to Guess My Word. A singular game of chance.")
