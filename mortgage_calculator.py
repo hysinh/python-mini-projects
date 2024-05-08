46b1d58e0a4de966ca4bb659105babbddb34db78
NgocDzung Smith <hysinh1@gmail.com>
2024-05-08 12:47:53 +0000

    Adds monthly interest calculation

--- mortgage_calculator.py
+++ mortgage_calculator.py
@@ -7,5 +7,8 @@ rate = (APR/100)/12
 loan_amt = int(input("Type in your loan amount: "))
 apr = float(input("type in your APR, annual percentage rate, (ex 4.2): "))
 rate = round(((apr / 100) / 12), 5)
-
+loan_length = int(input("Type in your loan length in years: "))
+months = loan_length * 12
+monthly_interest = round((loan_amt * rate), 3)
 print(rate)
+print(f"Your Monthly Interest payment: ${monthly_interest}")
